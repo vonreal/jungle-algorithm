212de85f1206313f925c12b06750945a9b6ecbfd
jiun <vonreal20@gmail.com>
2026-03-16 14:50:02 +0900

    feat(boj/#60): 첫 풀이

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,32 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+import sys
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+
+# print((A ** B) % C) 시간초과
+
+def custom_pow(base, top):
+    if top == 0:
+        return 1 % C
+    
+    base %= C
+
+    if top == 1:
+        return base % C
+    
+    half = custom_pow(base, top // 2) % C
+
+    if top % 2 == 0:
+        return (half * half) % C
+    else:
+        return (base * half * half % C) % C
+    
+print(custom_pow(A, B))
+
+
+'''
+시간제한: 0.5초
+메모리제한: 128 mb
+'''
