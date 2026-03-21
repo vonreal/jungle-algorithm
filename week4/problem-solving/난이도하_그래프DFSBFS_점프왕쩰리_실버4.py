30322ba0b302ca35803af15e3afe658eb78e655b
jiun <vonreal20@gmail.com>
2026-03-21 21:24:06 +0900

    solve(dfs/#89): boj 16173 점프왕쩰리 첫 풀이

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,23 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+maps = [[int(n) for n in input().split()] for _ in range(N)]
+x = y = 0
+
+def jump_king(x, y):
+    if x >= N or y >= N:
+        return False
+    if maps[x][y] == 0:
+        return False
+    if maps[x][y] == -1:
+        return True
+
+    right_move = jump_king(x, y+maps[x][y]) # 오른쪽으로
+    left_move = jump_king(x+maps[x][y], y) # 아래로
+
+    return right_move or left_move 
+        
+print("HaruHaru" if jump_king(x, y) else "Hing")
