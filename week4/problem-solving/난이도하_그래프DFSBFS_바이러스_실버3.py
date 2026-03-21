5ca3a3acf1b548a872ca56220e7676ce807a7693
jiun <vonreal20@gmail.com>
2026-03-21 20:28:18 +0900

    solve(bfs/#90): boj 2606 바이러스 첫 풀이

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,36 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+from collections import defaultdict, deque
+import sys
+input = sys.stdin.readline
+
+total_computers = int(input())
+total_linked_computers = int(input())
+
+adj_list = defaultdict(list)
+
+# O(N) 인접 리스트 삽입
+for _ in range(total_linked_computers):
+    n, v = map(int, input().split())
+
+    adj_list[n].append(v)
+    adj_list[v].append(n)
+
+def bfs(n):
+    queue = deque()
+    visited_set = set()
+
+    queue.append(n)
+    visited_set.add(n)
+
+    while queue:
+        v = queue.popleft()
+        for adj_node in adj_list[v]:
+            if adj_node not in visited_set:
+                queue.append(adj_node)
+                visited_set.add(adj_node)
+
+    print(len(visited_set) - 1)
+
+bfs(1)
+
