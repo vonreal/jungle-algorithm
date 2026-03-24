2b5ad0e9d4a0bfe42b45e6f1745237c98bf17304
jiun <vonreal20@gmail.com>
2026-03-24 10:54:50 +0900

    solve(bfs/#97): boj 2294 동전2

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,37 @@
 # BFS - 동전 2 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2294
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [0] * n
+
+# 코인 데이터 넣어두기
+for i in range(n):
+    coins[i] = int(input())
+
+def bfs():
+    q = deque()
+    visited_set = set()
+
+    q.append((0, 0))
+    visited_set.add(0)
+
+    count = 0
+
+    while q:
+        total, count = q.popleft()
+        for coin in coins:
+            add_total = total + coin
+            add_count = count + 1
+            if add_total == k:
+                return add_count
+            elif add_total > k:
+                continue
+            if add_total not in visited_set:
+                q.append((add_total, add_count))
+                visited_set.add(add_total)
+    return -1
+    
+print(bfs())
