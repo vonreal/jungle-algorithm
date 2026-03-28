e021c240707fa260272714b325cf5ae4f5ce7bee
jiun <vonreal20@gmail.com>
2026-03-28 13:54:06 +0900

    solve(dp/#122): boj 9084 동전

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -3,26 +3,68 @@
 import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-coins = []
+T = int(input())
+result = [0] * T
 
-for i in range(N):
-    coin = int(input())
-    if coin <= K:
-        coins.append(coin)
+for test_index in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
 
-coin_count = 0
-while K > 0:
-    temp_count = 0
-    use_coin = coins.pop()
-    temp_count += K // use_coin
-    K -= temp_count * use_coin
+    dp = [0] * (M + 1)
+    dp[0] = 1
 
-    coin_count += temp_count
+    for coin in coins:
+        for price in range(coin, M + 1):
+            dp[price] += dp[price - coin]
 
-print(coin_count)
+    result[test_index] = str(dp[M])
+
+print("\n".join(result))
+
 
 '''
-K원을 만드는데 필요한 동전 개수의 최솟값
-가장 큰 동전액수부터 시작해서 깎아내린다.
+모든 방법을 세는 방법
+
+입력)
+T: 테스트 케이스의 개수 (1 ~ 10)
+N: 동전의 가지 수 (1 ~ 20)
+
+출력)
+N가지 동전으로 금액 M을 만드는 모든 방법의 수
+
+1. 뭘 구하는가?
+N가지 동전으로 금액 M을 만드는 모든 방법의 수
+
+2. 어떤 알고리즘을 선택하는가?
+모든 경우의 수: DP, BFS, 백트래킹 (입력값 오버)
+
+3. 매핑
+- 저장해야하는 데이터 혹은 부분합으로 접근
+
+30원을 만드는 법
+1원, 5원, 10원
+1원 * 30개
+5원 1개, 1원 * 25개
+5원 2개, 1원 * 20개
+5원 3개, 1원 * 15개
+5원 4개, 1원 * 10개
+5원 5개, 1원 * 5개
+5원 6개
+10원 1개 + total[20]
+10원 2개 + total[10]
+10원 3개 + total[0]
+
+
+1원을 만드는 경우의 수
+2원을 만드는 경우의 수
+저장할 데이터는 금액을 만드는 경우의 수!
+최솟값부터 시작해서
+
+coins의 처음 값부터 시작해서 이전의 값(남은 금액)을 누적해서 가짓수를 저장한다.
+
+상태: 1원부터 ~ M까지 반복
+이웃: coins의 처음부터 끝까지 돌면서 가능한 조합 모두 처리
+
+"한 번에 하나의 선택만 한다"는 DP의 사고 전환
 '''
