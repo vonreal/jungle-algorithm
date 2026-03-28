186eddf4ea8c35713725f9e7cefd440a2988837f
jiun <vonreal20@gmail.com>
2026-03-28 20:25:06 +0900

    solve(greed/#121): boj 1541 잃어버린 괄호

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,14 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+import sys
+input = sys.stdin.readline
+
+target = input().split('-')
+result = 0
+
+result = int(sum(map(int, target[0].split('+'))))
+
+for datas in target[1:]:
+    result -= sum(map(int, datas.split('+')))
+
+print(result)
