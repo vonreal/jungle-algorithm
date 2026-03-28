e4f22a50c09c8cec08297981356078559c05c75e
jiun <vonreal20@gmail.com>
2026-03-28 11:47:32 +0900

    solve(dp/#119): boj 1904 01타일 나머지연산으로 해결

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -9,9 +9,9 @@ dp = [0] * (N + 2)
 dp[1], dp[2] = 1, 2
 
 for i in range(3, N+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
 
-print(dp[N] % 15746)
+print(dp[N])
 
 
 '''
