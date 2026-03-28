6aa425b7bdc417693d55fa2cc99c0bb2015e83f7
jiun <vonreal20@gmail.com>
2026-03-28 17:25:31 +0900

    solve(dp/#123): boj 9251 LCS

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,18 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+import sys
+input = sys.stdin.readline
+
+strA = input().rstrip()
+strB = input().rstrip()
+
+dp = [[0 for _ in range(len(strB) + 1)] for _ in range(len(strA) + 1)]
+
+for i in range(1, len(strA) + 1):
+    for j in range(1, len(strB) + 1):
+        if strA[i - 1] == strB[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(strA)][len(strB)])
