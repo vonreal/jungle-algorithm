6e7ce7710f2549bd26b4d6d9041f2d5bd0de87d4
jiun <vonreal20@gmail.com>
2026-03-30 13:13:36 +0900

    solve(greedy/#127): boj 1931 회의실 배정

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,23 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+meetings = []
+
+for _ in range(N):
+    start, end = map(int, input().split())    
+    meetings.append((start, end))
+
+meetings.sort(key= lambda x: (x[1], x[0]))
+
+available_meetings = 0
+before_end = 0
+
+for start, end in meetings:
+    if start >= before_end:
+        before_end = end
+        available_meetings += 1
+
+print(available_meetings)
